13d6e53a51b8eedcb5549dbc1f49a6282a954b97
Søren Gregersen <soren@emplate.it>
2022-04-18 11:42:04 +0200

    No longer extending MainCommand

--- Sources/TuistIncrement/Commands/Subcommands/IncrementBuild.swift
+++ Sources/TuistIncrement/Commands/Subcommands/IncrementBuild.swift
@@ -1,18 +1,16 @@
 import ArgumentParser
 
-extension MainCommand {
-    struct IncrementBuild: ParsableCommand {
-        static var configuration = CommandConfiguration(
-            commandName: "build",
-            abstract: "Increments the build number"
-        )
+struct IncrementBuild: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "build",
+        abstract: "Increments the build number"
+    )
 
-        func run() throws {
-            let file = File(path: "EmplateConsumer/InfoPlist.h")
+    func run() throws {
+        let file = File(path: "EmplateConsumer/InfoPlist.h")
 
-            let buildNumber = try file.readBuild()
+        let buildNumber = try file.readBuild()
 
-            try file.updateBuild(buildNumber + 1)
-        }
+        try file.updateBuild(buildNumber + 1)
     }
 }
--- Sources/TuistIncrement/Commands/Subcommands/IncrementVersion.swift
+++ Sources/TuistIncrement/Commands/Subcommands/IncrementVersion.swift
@@ -1,30 +1,28 @@
 import ArgumentParser
 import Foundation
 
-extension MainCommand {
-    struct IncrementVersion: ParsableCommand {
-        static var configuration = CommandConfiguration(
-            commandName: "version",
-            abstract: "Increments the version number"
-        )
+struct IncrementVersion: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "version",
+        abstract: "Increments the version number"
+    )
 
-        func run() throws {
-            let file = File(path: "EmplateConsumer/InfoPlist.h")
+    func run() throws {
+        let file = File(path: "EmplateConsumer/InfoPlist.h")
 
-            var version = try file.readVersion()
+        var version = try file.readVersion()
 
-            let currentYear = Calendar.current.component(.year, from: Date())
+        let currentYear = Calendar.current.component(.year, from: Date())
 
-            if version.year == currentYear {
-                // Increment the number if the year hasn't changed
-                version.number += 1
-            } else {
-                // Reset to current year and version 1 if the year has changed
-                version.year = currentYear
-                version.number = 1
-            }
-
-            try file.updateVersion(version)
+        if version.year == currentYear {
+            // Increment the number if the year hasn't changed
+            version.number += 1
+        } else {
+            // Reset to current year and version 1 if the year has changed
+            version.year = currentYear
+            version.number = 1
         }
+
+        try file.updateVersion(version)
     }
 }
